057a62b40dd4f1a1837e0597701f5bd787dfa040
Enzo Zamberlan <enzo2017.bra@gmail.com>
2024-06-10 18:42:04 -0300

    Criei um programa sobre eventos de teclas

---
+++ KeyEvents/index.js
@@ -0,0 +1,38 @@
+// eventListener = Listen for specific events to create interactive web pages
+//                      events: keydown, keyup
+//                      document.addEventListener(event, callback);
+
+const myBox = document.getElementById("myBox");
+const moveAmount = 50;
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", event => {
+    myBox.textContent = "😯";
+    myBox.style.backgroundColor = "tomato";
+})
+
+document.addEventListener("keydown", event => {
+    if (event.key.startsWith("Arrow")){
+
+        event.preventDefault();
+
+        switch(event.key){
+            case "ArrowUp":
+                y -= moveAmount;
+                break;
+            case "ArrowDown":
+                y += moveAmount;
+                break;
+            case "ArrowLeft":
+                x -= moveAmount;
+                break;
+            case "ArrowRight":
+                x += moveAmount;
+                break;      
+        }
+
+        myBox.style.top = `${y}px`;
+        myBox.style.left = `${x}px`;
+    }
+})
